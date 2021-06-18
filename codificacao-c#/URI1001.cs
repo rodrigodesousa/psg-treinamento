14e058a7f9cde008048e64d35d5bf48846775ea2
rodrigodesousa <rodrigudesousa@gmail.com>
2021-06-18 11:28:24 -0400

    inicio uri c#

---
+++ codificacao-c#/URI1001.cs
@@ -0,0 +1,15 @@
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int A, B;
+        A = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
+        int X = A + B;
+        Console.WriteLine("X = " + X);
+    }
+
+}
