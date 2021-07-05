d7ec176469ccbe66070e2da33a9ebabd44f5222d
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-07-05 00:23:49 -0400

    modelagem ecommerce de jogos

---
+++ codificacao-sql/encontro 15/lojaJogosConsultas.sql
@@ -0,0 +1,15 @@
+select * from formaPagamento;
+
+select * from cliente;
+
+select * from fornecedor;
+
+select * from categoria;
+
+select * from produto;
+
+select * from venda;
+
+select * from vendaProduto;
+
+
