20224482e8486a3ac77fc16c72b326d7b860306e
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-07-12 07:29:48 -0400

    alura projeto curso spring mvc

--- codificacao-sql/encontro 14/fluxoConsultas.sql
+++ codificacao-sql/encontro 14/fluxoConsultas.sql
@@ -19,6 +19,8 @@ inner join clienteFornecedor cf on fc.clientefornecedorid = cf.clientefornecedor
 where tipoTransacao = 'entrada' group by cf.nome, cf.clientefornecedorid;*/
 
 /*MAIOR VENDA EM UM PERIODO*/
-/*select max(valor) as 'maior valor' from fluxoCaixa
-where tipoTransacao = 'entrada' and
-datahora > '20210701 00:00:00' and datahora < '20210702 23:59:00';*/
+/*select cf.nome, max(fc.valor)  as 'maior valor' from fluxoCaixa fc
+inner join clienteFornecedor cf on fc.clientefornecedorid = cf.clientefornecedorid
+where fc.tipoTransacao = 'entrada' and
+fc.datahora > '20210701 00:00:00' and fc.datahora < '20210702 23:59:00'
+group by cf.nome;*/
