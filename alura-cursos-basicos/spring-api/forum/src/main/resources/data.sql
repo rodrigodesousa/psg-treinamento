645552b71a0d81d09908fa1456f386ce00c64a15
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-07-12 20:25:48 -0400

    Autenticando usuario

--- alura-cursos-basicos/spring-api/forum/src/main/resources/data.sql
+++ alura-cursos-basicos/spring-api/forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$IIpDQR6nStiPzm3WORYDvuvS9JbM8C1DWVWt4lMEhfbM3QJasQ8zy');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
